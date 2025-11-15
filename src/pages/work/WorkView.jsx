5aef5848fc7aa7885b8b6c480d7f512c7cec259c
paulovdev <fudevil123@gmail.com>
2025-11-15 16:56:28 -0300

    new pdate31

--- src/pages/work/WorkView.jsx
+++ src/pages/work/WorkView.jsx
@@ -169,12 +169,12 @@ const WorkView = () => {
           {/* Skeleton Loader */}
           <div className="flex flex-col gap-6">
             {/* Primeiro item grande */}
-            <div className="w-full h-[75vh] bg-[#E5E3DC] animate-pulse" />
+            <div className="w-full h-[75vh] bg-[#E5E3DC] animate-pulse max-ds:h-[550px] max-lg:h-[350px] max-md:h-[250px]" />
 
             {/* Grid de 2 colunas */}
             <div className="grid grid-cols-2 gap-4">
-              <div className="w-full h-[75vh] bg-[#E5E3DC] animate-pulse" />
-              <div className="w-full h-[75vh] bg-[#E5E3DC] animate-pulse" />
+              <div className="w-full h-[75vh] bg-[#E5E3DC] animate-pulse max-ds:h-[550px] max-lg:h-[350px] max-md:h-[250px]" />
+              <div className="w-full h-[75vh] bg-[#E5E3DC] animate-pulse max-ds:h-[550px] max-lg:h-[350px] max-md:h-[250px]" />
             </div>
           </div>
         </section>
