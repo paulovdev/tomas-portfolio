64e664bb33e3bed430720ee88737fb6287bb7972
paulovdev <fudevil123@gmail.com>
2025-11-16 08:26:47 -0300

    new pdate47 attention

--- client.js
+++ client.js
@@ -1,9 +1,10 @@
 import { createClient } from "@sanity/client";
 
-export const client = createClient({
+const client = createClient({
   projectId: import.meta.env.VITE_PUBLIC_SANITY_PROJECT_ID,
   dataset: import.meta.env.VITE_PUBLIC_SANITY_PROJECT_DATASET,
   apiVersion: "2024-01-01",
   useCdn: true,
   perspective: "published",
 });
+export default client;
