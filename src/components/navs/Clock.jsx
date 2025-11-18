fcb2768c57e245f7c5d1c7b923f15ff35b01aa24
paulovdev <fudevil123@gmail.com>
2025-11-17 21:06:42 -0300

    new: add time format 4

--- src/components/navs/Clock.jsx
+++ src/components/navs/Clock.jsx
@@ -2,42 +2,33 @@ import { useEffect, useState } from "react";
 
 const Clock = () => {
   const [time, setTime] = useState("");
+  const [time2, setTime2] = useState("");
+  const userLocale = navigator.language || "en-US";
   const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
   useEffect(() => {
     const updateClock = () => {
-      let formatted = new Date().toLocaleTimeString("en-US", {
+      const formatted = new Date().toLocaleTimeString(userLocale, {
         hour: "2-digit",
         minute: "2-digit",
         second: "2-digit",
-        hour12: usesAMPM(userTimeZone),
         timeZone: userTimeZone,
       });
 
-      // Remove pontos no AM/PM (a.m. → AM / p.m. → PM)
-      formatted = formatted
-        .replace(/a\.?m\.?/i, "AM")
-        .replace(/p\.?m\.?/i, "PM");
-
+      if (formatted >= "12:00:00") {
+        setTime2("PM");
+      } else {
+        setTime2("AM");
+      }
       setTime(formatted);
     };
 
     updateClock();
     const interval = setInterval(updateClock, 1000);
     return () => clearInterval(interval);
-  }, [userTimeZone]);
+  }, [userLocale, userTimeZone]);
 
-  return <div>{time}</div>;
+  return <div>{time + " " + time2}</div>;
 };
-
-function usesAMPM(zone) {
-  const test = new Date().toLocaleTimeString("en-US", {
-    hour: "numeric",
-    hour12: undefined,
-    timeZone: zone,
-  });
-
-  return /AM|PM|a\.?m\.?|p\.?m\.?/i.test(test);
-}
 
 export default Clock;
