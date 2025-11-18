4611d009363973b73a9f34c6f94356de2d5fb7d3
paulovdev <fudevil123@gmail.com>
2025-11-17 21:09:19 -0300

    new: add time format 5

--- src/components/navs/Clock.jsx
+++ src/components/navs/Clock.jsx
@@ -3,23 +3,29 @@ import { useEffect, useState } from "react";
 const Clock = () => {
   const [time, setTime] = useState("");
   const [time2, setTime2] = useState("");
+
   const userLocale = navigator.language || "en-US";
   const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
   useEffect(() => {
     const updateClock = () => {
-      const formatted = new Date().toLocaleTimeString(userLocale, {
+      const date = new Date();
+
+      const formatted = date.toLocaleTimeString(userLocale, {
         hour: "2-digit",
         minute: "2-digit",
         second: "2-digit",
         timeZone: userTimeZone,
+        hour12: false, // força **sempre** formato 24h pra evitar AM/PM oculto
       });
 
-      if (formatted >= "12:00:00") {
-        setTime2("PM");
-      } else {
-        setTime2("AM");
-      }
+      const hours = new Date().toLocaleString("en-US", {
+        hour: "2-digit",
+        hour12: false,
+        timeZone: userTimeZone,
+      });
+
+      setTime2(Number(hours) >= 12 ? "PM" : "AM");
       setTime(formatted);
     };
 
